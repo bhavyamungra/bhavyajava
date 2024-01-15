410dde26ef8513d0fb5db82449906122602847f6
bhvaya mungra <mungrabhavya@gmail.com>
2024-01-15 19:44:52 -0400

    checkout master

--- src/App.java
+++ src/App.java
@@ -6,7 +6,7 @@ public class App {
         myWindow = new JFrame();
         myWindow.setSize(300,200);
         myWindow.setTitle("opening the Windows......");
-        // myWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        myWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
 
 
