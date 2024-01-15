f540806338580cb1efe2154d842d590bd78a7734
bhvaya mungra <mungrabhavya@gmail.com>
2024-01-15 19:32:52 -0400

    fix: shut down automativally when JFramne closed

--- src/App.java
+++ src/App.java
@@ -4,5 +4,12 @@ public class App {
     public static void main(String[] args) throws Exception {
         JFrame  myWindow;
         myWindow = new JFrame();
+        myWindow.setSize(300,200);
+        myWindow.setTitle("opening the Windows......");
+        myWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+
+
+        myWindow.setVisible(true);
     }
 }
