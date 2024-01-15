d65b2cada7df52d911636e4133bf4e104984c4ee
bhvaya mungra <mungrabhavya@gmail.com>
2024-01-15 19:23:30 -0400

    constructed JFramne object

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import javax.swing.JFrame;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World!");
+        JFrame  myWindow;
+        myWindow = new JFrame();
     }
 }
